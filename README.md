0ed1ce2c7ed9aa0cae22eb9d8043f610e26eca9e
Junho <pinedance@gmail.com>
2024-05-28 14:37:45 +0900

    udpate codes

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# data-corpora-kanseki-SK3
-사고전서 Kanseki Repository 가운데 자부(子部)만 받아오기
+# corpora-kanseki-한적자부
+
+[ksnseki_repository](http://www.kanripo.org/)에 있는 자부(子部) 의가류(醫家類) 데이터
+
+* [漢リポ Kanseki Repository](http://www.kanripo.org/)
+* [漢リポ Kanseki Repository Github repo](https://github.com/kanripo)
+
+## 방법
+
+create `Kanseki_Repository.gitmodules` file
+
+```bash
+poetry run python build_gitmodules.py
+```
+
+clone submodules
+
+```bash
+bash git_clone_Kanseki_Repository.sh
+```
+
+## check
+
+```bash
+git submodule status
+```
+
+## update
+
+```bash
+git submodule
+git submodule update --remote
+```
+
+## Remove Submodule
+
+REF: [How do I revert my changes to a git submodule?](https://stackoverflow.com/a/27415757)
+
+```bash
+git submodule
+git submodule deinit <path_to_submodule>
+git rm <path_to_submodule>
+rm -rf .git/modules/<path_to_submodule>
+git commit -m "Removed submodule <path_to_submodule>"
+```
+
+remove all
+
+```bash
+git submodule
+git submodule foreach git submodule deinit
+git submodule foreach git rm
+rm -rf .git/modules
+git commit -m "Removed all submodules"
+```
+
+
+## RESET Submodules
+
+REF: [How do I revert my changes to a git submodule?](https://stackoverflow.com/a/27415757)
+
+```bash
+git submodule
+git submodule deinit -f .
+git submodule update --init
+```
+
+REF: https://unstop.com/blog/git-submodule
---
+++ git_clone_Kanseki_Repository.sh
@@ -0,0 +1,209 @@
+git submodule add "https://github.com/kanripo/KR3e0001.git" "의가류/KR3e0001_黃帝內經素問-唐-王冰" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0002.git" "의가류/KR3e0002_靈樞經-唐-王冰" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0003.git" "의가류/KR3e0003_王翰林集註黃帝八十一難經-明-" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0004.git" "의가류/KR3e0004_難經本義-元-滑壽" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0005.git" "의가류/KR3e0005_鍼灸甲乙經-晉-皇甫謐" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0006.git" "의가류/KR3e0006_金匱要畧論註-漢-張機" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0007.git" "의가류/KR3e0007_新編金匱要略方論-漢-" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0008.git" "의가류/KR3e0008_傷寒論注釋-漢-張機" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0009.git" "의가류/KR3e0009_新刊王氏脈經-晉-" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0010.git" "의가류/KR3e0010_肘後備急方-晉-葛洪" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0011.git" "의가류/KR3e0011_褚氏遺書-南齊-褚澄" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0012.git" "의가류/KR3e0012_巢氏諸病源候總論-隋-巢元方" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0013.git" "의가류/KR3e0013_備急千金要方-唐-孫思邈" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0014.git" "의가류/KR3e0014_銀海精微-唐-孫思邈" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0015.git" "의가류/KR3e0015_外臺秘要方-唐-王燾" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0016.git" "의가류/KR3e0016_顱顖經-宋-闕名" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0017.git" "의가류/KR3e0017_銅人鍼灸經-宋-闕名" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0018.git" "의가류/KR3e0018_明堂灸經--西方子" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0019.git" "의가류/KR3e0019_博濟方-宋-王袞" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0020.git" "의가류/KR3e0020_蘇沈良方-宋-蘇軾" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0021.git" "의가류/KR3e0021_壽親養老新書-宋-陳直" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0022.git" "의가류/KR3e0022_腳氣治法總要-宋-董汲" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0023.git" "의가류/KR3e0023_旅舍備要方-宋-董汲" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0024.git" "의가류/KR3e0024_素問入式運氣論奧-宋-劉溫舒" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0025.git" "의가류/KR3e0025_傷寒微旨論-宋-韓祗和" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0026.git" "의가류/KR3e0026_傷寒總病論-宋-龐安時" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0027.git" "의가류/KR3e0027_聖濟總錄纂要-宋-徽宗" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0028.git" "의가류/KR3e0028_證類本草-宋-唐愼微" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0029.git" "의가류/KR3e0029_重修政和經史證類備用本草-宋-" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0030.git" "의가류/KR3e0030_全生指迷方-宋-王貺" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0031.git" "의가류/KR3e0031_小兒衞生總散論方-宋-闕名" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0032.git" "의가류/KR3e0032_類證普濟本事方-宋-許叔微" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0033.git" "의가류/KR3e0033_太平惠民和劑局方-宋-陳師文" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0034.git" "의가류/KR3e0034_傳信適用方-宋-吳彥夔" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0035.git" "의가류/KR3e0035_衞濟寶書-宋-東軒居士" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0036.git" "의가류/KR3e0036_醫說-宋-張杲" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0037.git" "의가류/KR3e0037_鍼灸資生經-宋-王執中" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0038.git" "의가류/KR3e0038_婦人大全良方-宋-陳自明" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0039.git" "의가류/KR3e0039_太醫局諸科程文格-宋-何大任" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0040.git" "의가류/KR3e0040_產育寶慶集-宋-陳師聖" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0041.git" "의가류/KR3e0041_三因極一病證方論-宋-陳言" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0042.git" "의가류/KR3e0042_集驗背疽方-宋-李迅" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0043.git" "의가류/KR3e0043_濟生方-宋-嚴用和" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0044.git" "의가류/KR3e0044_產寶諸寶-宋-闕名" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0045.git" "의가류/KR3e0045_仁齋直指-宋-楊士瀛" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0046.git" "의가류/KR3e0046_急救仙方--闕名" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0047.git" "의가류/KR3e0047_素問玄機原病式-金-劉完素" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0048.git" "의가류/KR3e0048_宣明方論-金-劉完素" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0049.git" "의가류/KR3e0049_傷寒直格方-金-劉完素" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0050.git" "의가류/KR3e0050_保命集-金-張元素" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0051.git" "의가류/KR3e0051_儒門事親-金-張從正" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0052.git" "의가류/KR3e0052_內外傷辯惑論-元-李杲" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0053.git" "의가류/KR3e0053_脾胃論-元-李杲" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0054.git" "의가류/KR3e0054_蘭室祕藏-元-李杲" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0055.git" "의가류/KR3e0055_此事難知-元-王好古" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0056.git" "의가류/KR3e0056_醫壘元戎-元-王好古" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0057.git" "의가류/KR3e0057_湯液本章-元-王好古" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0058.git" "의가류/KR3e0058_瑞竹堂經驗方-元-沙圖穆蘇" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0059.git" "의가류/KR3e0059_世醫得效方-元-危亦林" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0060.git" "의가류/KR3e0060_格致餘論-元-朱震亨" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0061.git" "의가류/KR3e0061_局方發揮-元-朱震亨" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0062.git" "의가류/KR3e0062_金匱鈎玄-元-朱震亨" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0063.git" "의가류/KR3e0063_扁鵲神應鍼灸玉龍經-元-王國瑞" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0064.git" "의가류/KR3e0064_外科精義-元-齊德之" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0065.git" "의가류/KR3e0065_脉訣刊誤-元-戴啟宗" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0066.git" "의가류/KR3e0066_醫經溯洄集-元-王履" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0067.git" "의가류/KR3e0067_普濟方-明-朱橚" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0068.git" "의가류/KR3e0068_玉機微義-明-徐用誠" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0069.git" "의가류/KR3e0069_仁端錄-明-徐謙" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0070.git" "의가류/KR3e0070_薛氏醫案-明-薛己" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0071.git" "의가류/KR3e0071_推求師竟-明-戴原禮" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0072.git" "의가류/KR3e0072_針灸問對-明-汪機" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0073.git" "의가류/KR3e0073_外料理例-明-汪機" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0074.git" "의가류/KR3e0074_石山醫案-明-陳桷" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0075.git" "의가류/KR3e0075_名醫類案-明-江瓘" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0076.git" "의가류/KR3e0076_赤水元珠-明-孫一奎" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0077.git" "의가류/KR3e0077_醫旨緒餘-明-孫一奎" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0078.git" "의가류/KR3e0078_證治準繩-明-王肯堂" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0079.git" "의가류/KR3e0079_本草綱目-明-李時珍" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0080.git" "의가류/KR3e0080_奇經八脉攷-明-李時珍" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0081.git" "의가류/KR3e0081_瀕湖脉學-明-李時珍" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0082.git" "의가류/KR3e0082_傷寒論條辦-明-方有執" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0083.git" "의가류/KR3e0083_先醒齋廣筆記-明-繆希雍" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0084.git" "의가류/KR3e0084_神農本草經疏-明-繆希雍" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0085.git" "의가류/KR3e0085_類經-明-張介賓" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0086.git" "의가류/KR3e0086_景岳全書-明-張介賓" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0087.git" "의가류/KR3e0087_瘟疫論-明-吳有性" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0088.git" "의가류/KR3e0088_痎瘧論疏-明-廬之頤" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0089.git" "의가류/KR3e0089_本草乘雅半偈-明-廬之頤" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0090.git" "의가류/KR3e0090_御纂醫宗金鑑-清-吳謙" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0091.git" "의가류/KR3e0091_尚論篇-清-喻昌" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0092.git" "의가류/KR3e0092_醫門法律-清-喻昌" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0093.git" "의가류/KR3e0093_傷寒舌鑑-清-張登" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0094.git" "의가류/KR3e0094_傷寒兼證析義-清-張倬" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0095.git" "의가류/KR3e0095_絳雪園古方選註-清-王子接" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0096.git" "의가류/KR3e0096_續名醫類案-清-魏之琇" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0097.git" "의가류/KR3e0097_蘭臺軌範-清-徐大椿" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0098.git" "의가류/KR3e0098_神農本草經百种錄-清-徐大椿" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0099.git" "의가류/KR3e0099_傷寒類方-清-徐大椿" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0100.git" "의가류/KR3e0100_醫學源流論-清-徐大椿" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0101.git" "의가류/KR3e0101_本草經集注-梁-陶弘景" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0102.git" "의가류/KR3e0102_神農本草經--" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0103.git" "의가류/KR3e0103_新修本草-唐-蘇敬" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0104.git" "의가류/KR3e0104_本草新編-清-陳士鐸" -b "master" --depth 1 || true
+
+git submodule add "https://github.com/kanripo/KR3e0105.git" "의가류/KR3e0105_名醫別錄-梁-陶弘景" -b "master" --depth 1 || true
